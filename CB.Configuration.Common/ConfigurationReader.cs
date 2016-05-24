1a721e265155d1bc161e529ca4751524541d1f27
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-05-24 14:24:26 +0700

    First commit

---
+++ CB.Configuration.Common/ConfigurationReader.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+
+namespace CB.Configuration.Common
+{
+    public class ConfigurationReader<TConfigurationSection>
+        where TConfigurationSection: ConfigurationSection, new()
+    {
+        #region  Constructors & Destructor
+        public ConfigurationReader(string configSectionName)
+        {
+            ConfigurationSection = (TConfigurationSection)ConfigurationManager.GetSection(configSectionName) ??
+                                   new TConfigurationSection();
+        }
+        #endregion
+
+
+        #region  Properties & Indexers
+        public TConfigurationSection ConfigurationSection { get; set; }
+        #endregion
+    }
+}
