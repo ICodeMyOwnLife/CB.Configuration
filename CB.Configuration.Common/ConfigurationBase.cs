bc505551b8a1a83dbf51562241339579fe1e4454
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-06-10 20:24:41 +0700

    Add ConfigurationBase.Save

--- CB.Configuration.Common/ConfigurationBase.cs
+++ CB.Configuration.Common/ConfigurationBase.cs
@@ -6,9 +6,15 @@ namespace CB.Configuration.Common
     public class ConfigurationBase<TConfigurationSection>
         where TConfigurationSection: ConfigurationSection, new()
     {
+        #region Fields
+        private readonly string _configSectionName;
+        #endregion
+
+
         #region  Constructors & Destructor
         public ConfigurationBase(string configSectionName)
         {
+            _configSectionName = configSectionName;
             ConfigurationSection = (TConfigurationSection)ConfigurationManager.GetSection(configSectionName) ??
                                    new TConfigurationSection();
         }
@@ -17,6 +23,25 @@ namespace CB.Configuration.Common
 
         #region  Properties & Indexers
         public TConfigurationSection ConfigurationSection { get; set; }
+        #endregion
+
+
+        #region Methods
+        public void Save(ConfigurationUserLevel userLevel = ConfigurationUserLevel.None)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(userLevel);
+
+            var configurationSection = config.Sections[_configSectionName] as TConfigurationSection;
+            if (configurationSection != null)
+            {
+                config.Sections.Remove(_configSectionName);
+            }
+
+            config.Sections.Add(_configSectionName, ConfigurationSection);
+
+            ConfigurationSection.SectionInformation.ForceSave = true;
+            config.Save(ConfigurationSaveMode.Modified);
+        }
         #endregion
     }
 }
